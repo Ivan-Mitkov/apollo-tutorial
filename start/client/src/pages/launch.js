2da3993839c51a97d61d05029670250cee3ea402
unknown <ivan.mitkov@gmail.com>
2019-10-12 18:05:44 +0300

    Build a paginated list

--- start/client/src/pages/launch.js
+++ start/client/src/pages/launch.js
@@ -25,18 +25,45 @@ const GET_LAUNCHES = gql`
   }
 `;
 export default function Launches() {
-  const { data, loading, error } = useQuery(GET_LAUNCHES);
+  // To build a paginated list with Apollo, we first need to destructure the fetchMore
+  //function from the useQuery result object:
+  const { data, loading, error, fetchMore } = useQuery(GET_LAUNCHES);
   if (loading) return <Loading />;
   if (error) return <p>ERROR</p>;
 
   return (
     <Fragment>
       <Header />
-      {data.launches &&
-        data.launches.launches &&
-        data.launches.launches.map(launch => (
-          <LaunchTile key={launch.id} launch={launch} />
-        ))}
+      {data.launches && data.launches.hasMore && (
+        <Button
+        // Now that we have fetchMore, let's connect it to a Load More button 
+        //to fetch more items when it's clicked. To do this, we will need to specify
+        // an updateQuery function on the return object from fetchMore
+        // that tells the Apollo cache how to update our query with the new items we're fetching.
+          onClick={() =>
+            fetchMore({
+              variables: {
+                after: data.launches.cursor
+              },
+              updateQuery: (prev, { fetchMoreResult, ...rest }) => {
+                if (!fetchMoreResult) return prev;
+                return {
+                  ...fetchMoreResult,
+                  launches: {
+                    ...fetchMoreResult.launches,
+                    launches: [
+                      ...prev.launches.launches,
+                      ...fetchMoreResult.launches.launches
+                    ]
+                  }
+                };
+              }
+            })
+          }
+        >
+          Load More
+        </Button>
+      )}
     </Fragment>
   );
 }
