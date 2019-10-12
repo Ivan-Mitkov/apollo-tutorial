9c0f670aeea30b803a45650c44b1a8328ca43a4a
unknown <ivan.mitkov@gmail.com>
2019-10-12 17:40:27 +0300

    connect apolo client to react

---
+++ start/client/src/index.js
@@ -0,0 +1,39 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import gql from "graphql-tag";
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pages from './pages';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:4000/'
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
+// client
+//   .query({
+//     query: gql`
+//       query GetLaunch {
+//         launch(id: 56) {
+//           id
+//           mission {
+//             name
+//           }
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result));
+ReactDOM.render(
+    <ApolloProvider client={client}>
+      <Pages />
+    </ApolloProvider>, document.getElementById('root')
+  );
